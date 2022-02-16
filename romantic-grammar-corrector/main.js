b21c136ae1bc40bf07b635751c8b4cc0bdecc5de
lucky-victory <blizwonder25@gmail.com>
2022-02-16 22:18:05 +0100

    completed grammar corrector

--- romantic-grammar-corrector/main.js
+++ romantic-grammar-corrector/main.js
@@ -2,17 +2,57 @@ import React,{useRef,useState} from 'react';
 import ReactDOM from 'react-dom';
 
 export default function App(){
+   const [broken,setBroken]=useState(true);
+   const paraGraph=useRef();
+   const button=useRef();
+   const brokenWords=["vAlEntINe's!","VAleNtiNe's!","hapPY vAlEntINe DaY"];
+   const randomWord=brokenWords[Math.floor(Math.random() * brokenWords.length)];
+   const [text,setText]=useState(randomWord);
    
-   const [text,setText]=useState("vAlEntINe's!")
-   const correctGrammar=()=>{
-      
+
+   
+   const correctOrBreakGrammar=()=>{
+      paraGraph.current.classList.add('animate');
+         
+     if(broken){
+      window.setTimeout(()=>{
+      button.current.textContent='break grammar';
+     const textToTransform=text;
+    // get the first letter
+      let firstLetter = textToTransform.split('').shift();
+      // turn the first letter to uppercase
+      firstLetter = firstLetter.toUpperCase();
+      // get the remaining letters
+      const remainingLetters = textToTransform.split('').slice(1);
+   // transform the remaining letters to lowerCase
+      const lowercaseLetters =remainingLetters.map((letter) => letter.charAt(0).toLowerCase() + letter.slice(1)).join('');
+   
+   // set the transformed text
+    setText(`${firstLetter}${lowercaseLetters}`);
+    setBroken(false);
+    
+    
+      },2500);
+      return
+     }
+     window.setTimeout(()=>{
+        
+          button.current.textContent='correct grammar';
+ setBroken(true);
+     setText(randomWord);
+     },2500);
+     
+   }
+   
+   const removeClassName=()=>{
+      paraGraph.current.classList.remove('animate')
    }
    return (
    
               <div className="container">
             <h1>Romantic Grammar Corrector 💝</h1>
-      <p className="paragraph" >{text}</p>
-            <button id="btn" type="button"  onClick={correctGrammar}>Correct Grammar</button>
+      <p className="paragraph" onAnimationEnd={removeClassName} ref={paraGraph}>{text}</p>
+            <button id="btn" type="button"  onClick={correctOrBreakGrammar} ref={button}>Correct Grammar</button>
         </div>
 
       
